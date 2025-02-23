8cfe776af7c221d351d0a9f845e51536be8cc11d
AlexAltrex <alexaltrex@mail.ru>
2025-02-23 17:19:09 +0300

    4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <html lang="en">
         <body className={clsx(inter.className)}>
 
-        {/*<Preloader/>*/}
+        <Preloader/>
 
         <Header/>
 
